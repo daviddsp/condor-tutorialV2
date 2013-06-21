d4064b43a3bb77167d5ff256401a4439645b86e9
David Solórzano <abdsp22@gmail.com>
2013-06-21 12:44:58 -0430

    Modificaciones en la Temas

--- protected/views/temas/index.php
+++ protected/views/temas/index.php
@@ -19,6 +19,7 @@ $this->widget('bootstrap.widgets.TbTabs', array(
 		/* Lecciones */
 		array('label' => 'Lecciones', 
 			'items'=> array(
+				array('label'=>'Lista de Lecciones','url'=>array('/Lecciones/index')),
 				array('label'=>'Crear Lecciones','url'=>array('/Lecciones/create')),
 				array('label'=>'Gestión de Lecciones','url'=>array('/Lecciones/admin')),
 			),
@@ -30,6 +31,7 @@ $this->widget('bootstrap.widgets.TbTabs', array(
 		/* Contenido */
 		array('label' => 'Contenidos', 
 			'items'=> array(
+				array('label'=>'Lista de Contenidos','url'=>array('/Contenidos/index')),
 				array('label'=>'Crear Contenidos','url'=>array('/Contenidos/create')),
 				array('label'=>'Gestión de Contenidos','url'=>array('/Contenidos/admin')),
 			),
@@ -41,6 +43,7 @@ $this->widget('bootstrap.widgets.TbTabs', array(
 		/* Ejemplos */
 		array('label' => 'Ejemplos', 
 			'items'=> array(
+				array('label'=>'Lista de Ejemplos','url'=>array('/Ejemplos/index')),
 				array('label'=>'Crear Ejemplos','url'=>array('/Ejemplos/create')),
 				array('label'=>'Gestión de Ejemplos','url'=>array('/Ejemplos/admin')),
 			),
@@ -52,6 +55,7 @@ $this->widget('bootstrap.widgets.TbTabs', array(
 		/* Ejercicios */
 		array('label' => 'Ejercicios', 
 			'items'=> array(
+				array('label'=>'Lista de Ejercicios','url'=>array('/Ejercicios/index')),
 				array('label'=>'Crear Ejercicios','url'=>array('/Ejercicios/create')),
 				array('label'=>'Gestión de Ejercicios','url'=>array('/Ejercicios/admin')),
 			),
@@ -63,6 +67,7 @@ $this->widget('bootstrap.widgets.TbTabs', array(
 		/* Fuentes */
 		array('label' => 'Fuentes', 
 			'items'=> array(
+				array('label'=>'Lista de Fuentes','url'=>array('/Fuentes/index')),
 				array('label'=>'Crear Fuentes','url'=>array('/Fuentes/create')),
 				array('label'=>'Gestión de Fuentes','url'=>array('/Fuentes/admin')),
 			),
@@ -74,6 +79,7 @@ $this->widget('bootstrap.widgets.TbTabs', array(
 		/* Multimedia */
 		array('label' => 'Multimedia', 
 			'items'=> array(
+				array('label'=>'Lista de Multimedia','url'=>array('/Multimedia/index')),
 				array('label'=>'Crear Multimedia','url'=>array('/Multimedia/create')),
 				array('label'=>'Gestión de Multimedia','url'=>array('/Multimedia/admin')),
 			),
